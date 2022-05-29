86d006e3d9fa33dd0bfb8a215ef0d9278b97aaab
Sumit Pandey <sumitpandeyjnv@gmail.com>
2022-05-29 16:30:00 +0530

    Days1

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,9 +6,11 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login Page"),
-      ),
-    );
+        child: Column(
+      children: [
+        Image.asset("assets/images/login_logo.png"),
+        
+      ],
+    ));
   }
 }
