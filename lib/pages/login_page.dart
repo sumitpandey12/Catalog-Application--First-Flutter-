e03696bd23fd603bde3881d6c8e9c15969369b0a
Sumit Pandey <sumitpandeyjnv@gmail.com>
2022-06-01 22:53:47 +0530

    complate 1 hour

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,11 +6,56 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Column(
-      children: [
-        Image.asset("assets/images/login_logo.png"),
-        
-      ],
-    ));
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/login_logo.png",
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "Welcome",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Enter UserName",
+                  labelText: "UserName",
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Enter Password",
+                  labelText: "Password",
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Login"),
+                style: ButtonStyle(
+                  
+                ),
+                ),
+            ],
+          ),
+        ));
   }
 }
