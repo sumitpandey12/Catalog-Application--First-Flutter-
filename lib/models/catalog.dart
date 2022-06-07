d2a988c02d52501bcf7cee444ff079cec8730e7b
Sumit Pandey <sumitpandeyjnv@gmail.com>
2022-06-07 07:33:43 +0530

    ggj

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,31 @@
-class Item{
-  final String id;
+class CatalogModel {
+  static final items = [
+    Item(
+        1,
+        "Sumit Pandey",
+        "Android Developer",
+        "https://media-exp1.licdn.com/dms/image/C5603AQHw4o8wRSAQsQ/profile-displayphoto-shrink_200_200/0/1638617456666?e=2147483647&v=beta&t=hclciZ9f1nP_PFM2e5V-EZgMQL56HNHc8ube6b91r9g",
+        "#000000",
+        123),
+    Item(
+        1,
+        "Sumit Pandey",
+        "Android Developer",
+        "https://media-exp1.licdn.com/dms/image/C5603AQHw4o8wRSAQsQ/profile-displayphoto-shrink_200_200/0/1638617456666?e=2147483647&v=beta&t=hclciZ9f1nP_PFM2e5V-EZgMQL56HNHc8ube6b91r9g",
+        "#000000",
+        123),
+    Item(
+        1,
+        "Sumit Pandey",
+        "Android Developer",
+        "https://media-exp1.licdn.com/dms/image/C5603AQHw4o8wRSAQsQ/profile-displayphoto-shrink_200_200/0/1638617456666?e=2147483647&v=beta&t=hclciZ9f1nP_PFM2e5V-EZgMQL56HNHc8ube6b91r9g",
+        "#000000",
+        123),
+  ];
+}
+
+class Item {
+  final int id;
   final String name;
   final String desc;
   final String imageurl;
@@ -8,13 +34,3 @@ class Item{
 
   Item(this.id, this.name, this.desc, this.imageurl, this.color, this.price);
 }
-
-final products=[Item(
-  "001", 
-  "name", 
-  "desc", 
-  "https://media-exp1.licdn.com/dms/image/C5603AQHw4o8wRSAQsQ/profile-displayphoto-shrink_200_200/0/1638617456666?e=2147483647&v=beta&t=hclciZ9f1nP_PFM2e5V-EZgMQL56HNHc8ube6b91r9g", 
-  "#000000", 
-  123),
-  
-  ];
---
+++ lib/widgets/item_widget.dart
@@ -0,0 +1,29 @@
+import 'package:catalog_application1/models/catalog.dart';
+import 'package:flutter/material.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Card(
+        child: ListTile(
+          leading: Image.network(item.imageurl),
+          title: Text(item.name),
+          subtitle: Text(item.desc),
+          trailing: Text(
+            "\$${item.price}",
+            textScaleFactor: 1.2,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
